43143b0b08747df5d07673c1ac15519b7df15464
minhhuan20 <minhhuan20>
2022-05-04 16:35:20 +0700

    test

---
+++ Image-API/Image-API/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Image_API.Models;
+
+namespace Image_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost("[action]")]
+        public IActionResult UploadImage(List<IFormFile> files)
+        {
+            if (files.Count == 0)
+            {
+                return BadRequest();
+            }
+            string directoryPath = Path.Combine(_webHostEnvironment.ContentRootPath, "UploadImage");
+
+            foreach (var file in files)
+            {
+                string filePath = Path.Combine(directoryPath, file.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            return Ok("Upload Successful");
+        }
+    }
+}
---
+++ Image-API/Image-API/Migrations/20220504090038_Migrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Image_API.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    imagepath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    insertedOn = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+        }
+    }
+}
